d6445050ec84edd03a861448356d1d422978c4ae
AIME1202 <103973940+AIME1202@users.noreply.github.com>
2023-02-11 15:47:57 +0900

    Update ho.js

--- ho.js
+++ ho.js
@@ -1,5 +1,7 @@
-function hololive(){if(document.getElementById('kotae').value=='湊あくあ'){
+function hololive(){
+  if(document.getElementById('kotae').value=='湊あくあ'){
   alert('正解！');
+  }
 else {
  alert ('はずれ!もう一度hololiveの公式サイトを見てみよう');
  }
