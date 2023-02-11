9b8b33c56f943b3be983fc28bf1eecbb1773c201
AIME1202 <103973940+AIME1202@users.noreply.github.com>
2023-02-11 15:45:22 +0900

    Update V.js

--- V.js
+++ V.js
@@ -1,4 +1,4 @@
-function ぶいすぽっ！！(){if(document.getElementById('kotae2').value=='橘ひなの'){
+function ぶいすぽっ(){if(document.getElementById('kotae2').value=='橘ひなの'){
   alert('正解！');
 else {
  alert ('はずれ!もう一度ぶいすぽっ！！の公式サイトを見てみよう');
